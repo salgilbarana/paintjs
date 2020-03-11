561b30c809584271bf316a2a35e7d07deee4fd34
salgilbarana <ychcom@naver.com>
2020-03-11 16:02:30 +0900

    초기화 버튼 추가

--- app.js
+++ app.js
@@ -4,18 +4,16 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const clean = document.getElementById("init");
 
-const INITIAL_COLOR = "#2c2c2c";
+const INITIAL_COLOR = "white";
 const CANVAS_SIZE = 700;
-
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
-
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
-
 
 let painting = false;
 let filling = false;
@@ -40,69 +38,84 @@ function onMouseMove(event) {
   }
 }
 
-function handleColorClick(event){
-const color = event.target.style.backgroundColor;
-ctx.strokeStyle = color;
-ctx.fillStyle = color;
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
-function handleRangeChange(event){
+function handleRangeChange(event) {
   const size = event.target.value;
   ctx.lineWidth = size;
 }
 
-function handleModeClick(){
-  if(filling === true){
+function handleModeClick() {
+  if (filling === true) {
     filling = false;
-    mode.innerText = "Fill"
-  }else{
+    mode.innerText = "Fill";
+  } else {
     filling = true;
-    mode.innerText = "paint"
+    mode.innerText = "paint";
     ctx.fillStyle = ctx.strokeStyle;
   }
 }
 
 function handleCanvasClick() {
-  if(filling)
-  {
-  ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   }
 }
 
-function handleCM(event){ // 마우스 우클릭 방지 
+function handleCM(event) {
+  // 마우스 우클릭 방지
   event.preventDefault();
 }
 
-function handleSaveClick(){
+function handleSaveClick() {
   const image = canvas.toDataURL("image/png");
   const link = document.createElement("a");
   link.href = image;
   link.download = "PaintJS[✒]"; // <a download= />
   link.click();
-  console.log(link);
 }
+
+function handleInitClick() {
+  ctx.fillStyle = INITIAL_COLOR;
+  ctx.lineWidth = 2.5;
+  range.value = 2.5;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
-  canvas.addEventListener("contextmenu", handleCM)
+  canvas.addEventListener("contextmenu", handleCM);
   // 마우스 우클릭 방지
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick)
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
 );
 
-if(range)
-{
+if (range) {
   range.addEventListener("input", handleRangeChange);
 }
 
-if(mode){
+if (mode) {
   mode.addEventListener("click", handleModeClick);
 }
 
-if(saveBtn) {
+if (saveBtn) {
   saveBtn.addEventListener("click", handleSaveClick);
+}
+
+if (clean) {
+  clean.addEventListener("click", handleInitClick);
 }
